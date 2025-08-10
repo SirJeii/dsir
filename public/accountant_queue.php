cb7b9f65068d6d9ecb020655d63570d27b3e7811
JohnDelinquent-Kembo <donkevinudaundo.3dclass@gmail.com>
2025-08-10 23:43:46 +0800

    Update accountant_queue.php

--- public/accountant_queue.php
+++ public/accountant_queue.php
@@ -9,13 +9,13 @@ $user = $_SESSION['user'];
   <meta charset="utf-8">
   <title>Accountant Queue</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/dsir/public/css/style.css">
 </head>
 <body>
 <nav class="navbar navbar-light bg-white shadow-sm">
   <div class="container">
     <span class="navbar-brand mb-0 h5">Accountant – Pending Reports</span>
-    <a class="btn btn-outline-danger btn-sm" href="/logout.php">Logout</a>
+    <a class="btn btn-outline-danger btn-sm" href="/dsir/public/logout.php">Logout</a>
   </div>
 </nav>
 
@@ -55,6 +55,7 @@ $user = $_SESSION['user'];
   </div>
 </div>
 
-<script src="/js/accountant.js"></script>
+<script src="/dsir/public/js/accountant.js"></script>
 </body>
 </html>
+
